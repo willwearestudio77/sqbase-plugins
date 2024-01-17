4f27f3276821441369aef9ddff169da53639eb0b
willwearestudio77 <108265499+willwearestudio77@users.noreply.github.com>
2024-01-17 13:16:47 -0800

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Find all elements with the class slide-down-button
+    var slideDownButtons = document.querySelectorAll('.slide-down-button');
+    
+    // Iterate through each slide-down-button
+    slideDownButtons.forEach(function (button) {
+        // Get the closest parent element with the class page-section
+        var pageSection = button.closest('.page-section');
+        
+        // Get the next sibling element with the class page-section
+        var sectionBelow = pageSection.nextElementSibling;
+
+        // Hide the section below
+        if (sectionBelow) {
+            sectionBelow.classList.add('close');
+          pageSection.classList.add('toggle-section')
+        }
+    });
+});
+
+function toggleSection(button) {
+    // Get the closest parent element with the class page-section
+    var pageSection = button.closest('.page-section');
+
+    // Get the next sibling element with the class page-section
+    var sectionBelow = pageSection.nextElementSibling;
+
+    // Toggle the display property
+    if (sectionBelow){
+        sectionBelow.classList.toggle('close');
+          sectionBelow.classList.toggle('open');
+    } 
+}
